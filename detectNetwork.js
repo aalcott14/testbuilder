deb1aac9b22ffe910f365309e23abf5558e1001f
Adam Alcott <aalcott14@gmail.com>
2017-03-06 09:16:31 -0600

    Add Switch and China UnionPay

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,21 +13,35 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-  var prefix = cardNumber.substring(0,4);
+  var prefix1 = cardNumber.substring(0,1);
+  var prefix2 = cardNumber.substring(0,2);
+  var prefix3 = cardNumber.substring(0,3);
+  var prefix4 = cardNumber.substring(0,4);
+  var prefix6 = cardNumber.substring(0,6);
   var length = cardNumber.length;
-  if ((prefix.substring(0,2) === '38' || prefix.substring(0,2) === '39') && length === 14) {
+  
+  if ((prefix2 === '38' || prefix2 === '39') && length === 14) {
   	return 'Diner\'s Club';
-  } else if ((prefix.substring(0,2) === '34' || prefix.substring(0,2) === '37') && length === 15) {
+  } else if ((prefix2 === '34' || prefix2 === '37') && length === 15) {
   	return "American Express";
-  } else if (prefix.substring(0, 1) === '4' && (length === 13 || length === 16 || length === 19)) {
+  } else if (prefix1 === '4' && (length === 13 || length === 16 || length === 19)) {
   	return 'Visa';
-  } else if ((prefix.substring(0,2) === '51' || prefix.substring(0,2) === '52' || prefix.substring(0,2) === '53' || prefix.substring(0,2) === '54' || prefix.substring(0,2) === '55') && length === 16) {
+  } else if ((prefix4 = '4903' || prefix4 === '4905' || prefix4 === '4911' || prefix4 === '4936' || prefix6 === '564182' || prefix6 === '633110' || prefix4 === '6333' || prefix4 === '6759') && (length === 16 || length === 18 || length === 19)) {
+    return 'Switch';
+  } else if ((prefix2 === '51' || prefix2 === '52' || prefix2 === '53' || prefix2 === '54' || prefix2 === '55') && length === 16) {
   	return 'MasterCard';
-  } else if ((prefix === '6011' || prefix.substring(0,3) === '644' || prefix.substring(0,3) === '645' || prefix.substring(0,3) === '646' || prefix.substring(0,3) === '647' || prefix.substring(0,3) === '648' || prefix.substring(0,3) === '649' || prefix.substring(0,2) === '65') && (length === 16 || length === 19)) {
+  } else if ((prefix4 === '6011' || prefix3 === '644' || prefix3 === '645' || prefix3 === '646' || prefix3 === '647' || prefix3 === '648' || prefix3 === '649' || prefix2 === '65') && (length === 16 || length === 19)) {
   	return 'Discover';
-  } else if ((prefix === '5018' || prefix === '5020' || prefix === '5038' || prefix === '6304') && (length > 11 && length < 20)) {
+  } else if ((prefix4 === '5018' || prefix4 === '5020' || prefix4 === '5038' || prefix4 === '6304') && (length > 11 && length < 20)) {
   	return 'Maestro';
+  } 
+
+  for (var i = 622126; i <= 622925; i++) {
+    if ((Number(prefix6) === i || prefix3 === '624' || prefix3 === '625' || prefix3 === '626' || prefix4 === '6282' || prefix4 === '6283' || prefix4 === '6284' || prefix4 === '6285' || prefix4 === '6286' || prefix4 === '6287' || prefix4 === '6288') && (length === 16 || length === 17 || length ===18 || length === 19)) {
+      return 'China UnionPay';
+    } 
   }
-};
+
+  };
 
 
